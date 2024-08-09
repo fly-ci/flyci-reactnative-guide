0d83fe10a0fd4d5b1d070cc82b712ed3b96a6bd8
radeva <vesita@gmail.com>
2024-08-09 11:09:40 +0300

    add a simple test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,9 @@
 
 import 'react-native';
 import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import { it, expect } from '@jest/globals';
 import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import { it } from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -20,4 +19,12 @@ it('should render content correctly', () => {
   const appContent = renderer.create(<App />).toJSON();
 
   expect(appContent).toMatchSnapshot();
+});
+
+it('should render Step One', async () => {
+  render(<App />);
+
+  const stepOne = await screen.findByText('Step One');
+
+  expect(stepOne).toHaveTextContent('Step One');
 });
